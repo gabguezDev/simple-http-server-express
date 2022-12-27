ad903d28daebf479f5131586ed30287374412a40
Gabriel <gabriel.dominguez@nextgo.io>
2022-12-27 20:40:44 -0300

    webserver ready

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const express = require("express");
+const hbs = require("hbs");
+require("dotenv").config();
+
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
+
+// hbs.registerHelper("helper_name", function (options) {
+// 	return "helper value";
+// });
+hbs.registerPartials(__dirname + "/views/partials", function (err) {});
+
+app.set("view engine", "hbs");
 
 app.use(express.static("public"));
 
-/* app.get("/", (req, res) => {
-	res.sendFile(__dirname + "public/index.html");
-}); */
+app.get("/", (req, res) => {
+	res.render("home", { titulo: "Curso de Node", nombre: "Gabriel D." });
+});
+
 app.get("/generic", (req, res) => {
-	res.sendFile(__dirname + "/public/generic.html");
+	res.render(
+		"generic" /* , { titulo: "Curso de Node", nombre: "Gabriel D." } */
+	);
+	// res.sendFile(__dirname + "/public/generic.html");
 });
 app.get("/elements", (req, res) => {
-	res.sendFile(__dirname + "/public/elements.html");
+	res.render(
+		"elements" /* , { titulo: "Curso de Node", nombre: "Gabriel D." } */
+	);
+	// res.sendFile(__dirname + "/public/elements.html");
 });
 
 app.listen(port, () => {
